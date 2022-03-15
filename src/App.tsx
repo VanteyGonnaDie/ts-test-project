4761ead046a5e875b3d7bf9b6a3f2cfb561b952c
Ivan Dubinin <vanteygonnadie@gmail.com>
2022-03-15 16:35:30 +0300

    added bootstrap and sass, fix for input

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import InputForm from './components/InputForm';
 import NoteList from './components/NoteList';
 import AppButton from './components/UI/Button/AppButton';
 import AppInput from './components/UI/Input/AppInput';
-import './styles/App.css';
+// import './styles/App.css';
+import './styles/custom.sass';
 
 function App() {
   return (
