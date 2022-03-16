c3f0753b020e3f8a487254e0d113263be6131fea
Ivan Dubinin <vanteygonnadie@gmail.com>
2022-03-16 23:29:32 +0300

    fixes on input

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { notesList } from '../models/notes'
 import { RootState, store } from '../store'
@@ -13,24 +13,28 @@ const InputForm = () => {
   const {dispatch} = store
   const state = { text: '' }
     
-        const handleChange = (event) => {
+        const handleChange = event => {
             state.text  = event.target.value
             console.log(state.text)
-            console.log(typeof(state.text))
+            console.log(state)
           }
 
         const handleSubmit = event => {
             event.preventDefault();
-            dispatch.notesList.addNote(state.text)    
-            console.log(store.getState())
-            console.log(notesList.state)
-            // dispatch.myTodos.addNote(myNote)
-
+            console.log(state)
+            console.log(state.text)
+            dispatch.notesList.addNote(state.text) 
+            console.log(state)
+            console.log(store.getState());
+            (document.getElementById('MainInput') as HTMLInputElement).value = '';
+            
+            
         }
+    console.log(state)
     
   return (
     <form className='input-form' onSubmit={handleSubmit}>
-        <input placeholder='Введите заметку' type="text"  onChange={handleChange} />
+        <input id='MainInput' placeholder='Введите заметку' type="text"  onChange={handleChange} />
         <button type='submit'>Добавить заметку</button>
     </form>
   )
